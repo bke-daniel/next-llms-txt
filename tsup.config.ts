8eaa8962b639cafe551b974f332456eff362ca20
Daniel Birke <daniel.birke@apcoa.de>
2025-11-11 12:06:13 +0100

    v3.3.4 - esm only

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  format: ['cjs', 'esm'],
+  format: ['esm'],
   tsconfig: './tsconfig.json',
   dts: true,
   splitting: false,
-  external: ['react'],
+  external: ['react', 'react-dom', 'next', '@babel/parser', '@babel/traverse', '@babel/generator', '@babel/types', 'debug'],
+  noExternal: [],
   sourcemap: true,
   clean: true,
   outExtension: ({ format }) => ({
