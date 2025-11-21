05d2a54aa48d83b7290802450eae78586848ab34
Daniel Birke <daniel.birke@apcoa.de>
2025-11-21 13:16:11 +0100

    removed douplicated folder

--- packages/demo-server/src/app/app/no-exports/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function NoExportAtAllPage() {
-  return (
-    <>
-      <h1>Page with no exports</h1>
-      <p>This page has no exports at all - should generate warnings</p>
-      <a href="/no-exports.html.md">No Exports Markdown</a>
-    </>
-  )
-}
